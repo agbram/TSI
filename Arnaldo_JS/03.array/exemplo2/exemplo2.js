1631c1c5d45dad3b7cf0ba68549ebffd3242aebb
Gustavo Braga <agbram2005@gmail.com>
2025-09-22 22:16:34 -0300

    exericios realizado em js sobre array

---
+++ Arnaldo_JS/03.array/exemplo2/exemplo2.js
@@ -0,0 +1,19 @@
+const  times = ['Ceará', 'Corinthians', 'Santos', 'Palmeiras'];
+
+//adicionar time no fim do array
+
+times.push('Grêmio');
+
+console.log(times);
+
+//adicionar no começo do array
+
+times.unshift("Mirassol");
+console.log(times);
+
+//remover ultimo elemento do array
+console.log(times.pop());
+
+//remover o primeiro elemento do array
+
+console.log(times.shift());
